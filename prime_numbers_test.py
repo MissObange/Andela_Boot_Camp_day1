8c9a188ea4b8b531dbefaf25f85b90e6bdf997f6
MissObange <lindy.sharon1@gmail.com>
2017-03-10 02:43:46 +0300

    unnitest for prime numbers code

---
+++ prime_numbers_test.py
@@ -0,0 +1,18 @@
+import unittest
+import prime_numbers
+
+class SolutionTest(unitest.testCase):
+	def test_1(self):
+		self.assertTrue(prime_numbers(i,n,%)0)
+
+	def test_2(self):
+		self.assertTrue(primenumbers(i,1,=)0)
+
+	def test_3(self):
+		self.assertTrue(prime_numbers(i,1,%)0)
+
+	def test_4(self):
+		self.assertTrue(prime_numbers(i, i, %)0)
+
+	def test_5(self):
+		self.assertTrue(prime_numbers(i, num, %)0)
